ac7821a8d075f3f359e5f6166922d414ad7c5fac
Jean Silveira <silveirajeans@gmail.com>
2021-01-27 21:27:34 -0300

    setup heroku

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  console.log(`Server up and running on PORT ${port}`);
+});
